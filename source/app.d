1f6e42d953b876d1608bf006227b4913133010d2
KVetal <kvetal@mail.ru>
2018-12-26 22:26:48 +0500

    add mp3player module

--- source/app.d
+++ source/app.d
@@ -1,2 +1,9 @@
-void main(){
+import playlist;
+import mp3player;
+import types;
+
+void main()
+{
+	mp3player mplay = new mp3player();
+	
 }
---
+++ source/types.d
@@ -0,0 +1,76 @@
+module types;
+
+///
+struct PlayListItem {
+	/** Полное имя файла включая путь*/
+	string fullFileName;
+	/** Имя самого фала и расширение*/
+	string shortFileName;
+	/** Длительность в секундах*/
+	long duration = -1;
+	/** Инфо*/
+	string info;
+	}
+
+enum string bom = [0xef,0xbb,0xbf];
+
+alias soundStatus = int;
+enum {
+    Stopped,
+    Paused,
+    Playing
+}
+
+///Интерейс плейлиста
+interface IPlaylist {
+	/** Загрузить плейлист из файла m3u8*/
+	bool loadFromPLFile(string fileName);
+	/** Сохранить плейлист в файл m3u8*/
+	bool saveToPLFile(string fileName);
+	/** Добавить аудио файл в плейлист*/
+	void addTrackFromFile(string fileName);
+	/** Добавить трек в конец плейлиста*/
+	void addTrack(PlayListItem info);
+	/** Удалить файл из плейлиста*/
+	bool removeTrack(size_t index);
+	/** Получить номер текущего трека*/
+	size_t getCurrentTrackIndex();
+	/** Установить текущий трек*/
+	void setCurrentTrackIndex(size_t index);
+	/** Получить трек по нидексу*/
+	PlayListItem getTrack(size_t index);
+	/** Изменить инфо о треке*/
+	void setTrackInfo(size_t index, PlayListItem info);
+	/** Получить текущий трек*/
+	PlayListItem getCurrentTrack();
+	/** переключить текущий трек на следующий и получить его данные, если выключен loop флаг,
+	После последнего треке возвращает null*/
+	PlayListItem next();
+	/** переключить текущий трек на предыдущий и получить его данные,  если выключен loop флаг,
+	После первого трека возвращает null*/
+	PlayListItem prev();
+	/** установить флаг циклического перебора треков*/
+	void setLoop(bool flag);
+	/** получить флаг циклического перебора треков*/
+	bool getLoop();
+	/** Размер плейлиста*/
+	@property size_t length();
+	/** Получить копию плейлиста в виде массива структур типа PlayListItem*/
+	PlayListItem[] getArray();
+	/** Передать в класс массив записаей типа PlayListItem.
+	Будет передана ссылка, изменения исходнго массива будут влиять на плейлист, 
+	если это не нужно передавайте PlayListItem[].idup.*/
+	void setArray(PlayListItem[] playlist);
+}
+
+interface Player {
+	bool openFromFile(string fileName);
+	void play();
+	void pause();
+	void setVolume(float);
+	float getVolume();
+	void setPlayingOffset(long offset);
+	long getPlayingOffset();
+	soundStatus getStatus();
+	}
+
